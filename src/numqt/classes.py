4ea28754c0a1c60a1588ca48ff377ecf787bea45
Francesco Diotallevi <78257415+OtiDioti@users.noreply.github.com>
2025-07-14 09:20:38 +0200

    Update classes.py

--- src/numqt/classes.py
+++ src/numqt/classes.py
@@ -1042,10 +1042,10 @@ class Hamiltonian:
             if hasattr(H_td, 'toarray') and False: # this will never run!!!!!!!!!!
                 H_td_dense = H_td.toarray()
             else:
-                H_td_dense = H_td.tocsc()
+                H_td_dense = H_td
                 
             H_temp = H0_dense + H_td_dense
-            U = expm(-1j * dt_over_hbar * H_temp) @ U
+            U = expm(-1j * dt_over_hbar * H_temp.tocsc()) @ U
         
         # Compute Floquet Hamiltonian: HF = (i * hbar / period) * log[U]
         HF = 1j * hbar / period * logm(U.toarray())
